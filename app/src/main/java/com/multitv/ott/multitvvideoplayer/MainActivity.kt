88b9703c02f865bbd771d5689293f46a21c38421
vikram.sharma <vikram.sharma@multitvsolution.com>
2022-07-13 14:23:35 +0530

    drm sdk integrated in video player

--- app/src/main/java/com/multitv/ott/multitvvideoplayer/MainActivity.kt
+++ app/src/main/java/com/multitv/ott/multitvvideoplayer/MainActivity.kt
@@ -31,6 +31,7 @@ class MainActivity : AppCompatActivity(), VideoPlayerSdkCallBackListener {
             )
         )
         startPlayer()
+
     }
 
     override fun onPictureInPictureModeChanged(
