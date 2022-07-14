f9159edcd6fd5e4d9fc2a754c56952cbc92c49ab
vikram.sharma <vikram.sharma@multitvsolution.com>
2022-07-14 14:41:20 +0530

    short video player implemented

--- app/src/main/java/com/multitv/ott/multitvvideoplayer/VikramVideoPlayerTestActivity.kt
+++ app/src/main/java/com/multitv/ott/multitvvideoplayer/VikramVideoPlayerTestActivity.kt
@@ -11,8 +11,8 @@ import com.multitv.ott.multitvvideoplayer.utils.CommonUtils
 import com.multitv.ott.multitvvideoplayer.utils.ContentType
 import com.multitv.ott.multitvvideoplayer.utils.ScreenUtils
 
-class MainActivity : AppCompatActivity(), VideoPlayerSdkCallBackListener {
-    private var vikramExoVideoPlayer: MultiTvPlayerSdk? = null
+class VikramVideoPlayerTestActivity : AppCompatActivity(), VideoPlayerSdkCallBackListener {
+    private var vikramExoVideoPlayer: ShortVideoPlayer? = null
     private var token =
         "eyJkcm1fdHlwZSI6IldpZGV2aW5lIiwic2l0ZV9pZCI6IkpLSkciLCJ1c2VyX2lkIjoiYWRtaW5AdGVjaG1qYXBhbi5jb20iLCJjaWQiOiJjb250ZW50XzEwMDQiLCJwb2xpY3kiOiIyamlUNjFpbFNnRVp4Z01zcng4RWZSVVh4TFFUTHdyWEVtUld5bGNzNmZPbHhQNkZcLzk2VWpXR3g1ZmVkU0NEWCs0NHVMeUxZUCtRZjNCYkVpaWhpWFJSNkZxc0FRaUs0Z3dJNkhYZzZ5dlpVdFk5SG1PNjRPTmNpWUQ4ZG9Sbm8iLCJ0aW1lc3RhbXAiOiIyMDIyLTA3LTA1VDA5OjQ0OjU5WiIsInJlc3BvbnNlX2Zvcm1hdCI6Im9yaWdpbmFsIiwiaGFzaCI6ImVDS0tGWkpoNHYxeHBvdHU1MTliUnpVZkJJcFJIUXMwcW5iekRXM2szTU09Iiwia2V5X3JvdGF0aW9uIjpmYWxzZX0="
 
@@ -57,8 +57,8 @@ class MainActivity : AppCompatActivity(), VideoPlayerSdkCallBackListener {
           )*/
         // vikramExoVideoPlayer?.setContentFilePath("http://d34i7be7c53ukk.cloudfront.net/1004/1004_62ac86b418c6a/dash/stream.mpd")
         vikramExoVideoPlayer?.setContentFilePath("http://dpvruylq68d5u.cloudfront.net/1048/1048_624c3ec915773/1048_624c3ec915773_master.m3u8")
-        vikramExoVideoPlayer?.setSubtitleVideoUri("https://multitvott.s3.ap-south-1.amazonaws.com/subtitle.srt")
-        vikramExoVideoPlayer?.setPreRollAdUrl("https://pubads.g.doubleclick.net/gampad/ads?iu=/21775744923/external/single_ad_samples&sz=640x480&cust_params=sample_ct%3Dlinear&ciu_szs=300x250%2C728x90&gdfp_req=1&output=vast&unviewed_position_start=1&env=vp&impl=s&correlator=")
+        // vikramExoVideoPlayer?.setSubtitleVideoUri("https://multitvott.s3.ap-south-1.amazonaws.com/subtitle.srt")
+        // vikramExoVideoPlayer?.setPreRollAdUrl("https://pubads.g.doubleclick.net/gampad/ads?iu=/21775744923/external/single_ad_samples&sz=640x480&cust_params=sample_ct%3Dlinear&ciu_szs=300x250%2C728x90&gdfp_req=1&output=vast&unviewed_position_start=1&env=vp&impl=s&correlator=")
         vikramExoVideoPlayer?.setMultiTvVideoPlayerSdkListener(this)
         vikramExoVideoPlayer?.prepareVideoPlayer()
     }
