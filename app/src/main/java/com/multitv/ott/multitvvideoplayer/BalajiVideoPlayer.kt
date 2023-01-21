aa6eb09e033391341e20ccc265acf6d8f1ee729c
vikram.sharma <vikram.sharma@multitvsolution.com>
2023-01-21 18:03:31 +0530

    vikr

--- app/src/main/java/com/multitv/ott/multitvvideoplayer/BalajiVideoPlayer.kt
+++ app/src/main/java/com/multitv/ott/multitvvideoplayer/BalajiVideoPlayer.kt
@@ -1420,8 +1420,8 @@ class BalajiVideoPlayer(
     }
 
     override fun onScrubStart(previewBar: PreviewBar) {
-        previewFrameLayout.visibility = VISIBLE
-        previewTimeBar.showPreview()
+      //  previewFrameLayout.visibility = VISIBLE
+       // previewTimeBar.showPreview()
         pauseVideoPlayer()
     }
 
@@ -1434,11 +1434,11 @@ class BalajiVideoPlayer(
     }
 
     override fun onScrubStop(previewBar: PreviewBar) {
-        previewFrameLayout.visibility = INVISIBLE
+       // previewFrameLayout.visibility = INVISIBLE
         if (mMediaPlayer != null) {
             seekTo(previewBar.progress.toLong())
         }
-        previewTimeBar.hidePreview()
+        //previewTimeBar.hidePreview()
         resumeVideoPlayer()
     }
 
@@ -1446,8 +1446,8 @@ class BalajiVideoPlayer(
     override fun loadPreview(currentPosition: Long, max: Long) {
         Log.e("Video Sprite::::", "Url:::" + spriteImageUrl)
         pauseVideoPlayer()
-        previewFrameLayout.visibility = View.VISIBLE
-        previewTimeBar.showPreview()
+       // previewFrameLayout.visibility = View.VISIBLE
+       // previewTimeBar.showPreview()
         Glide.with(previewImageView)
             .load(spriteImageUrl)
             .override(Target.SIZE_ORIGINAL, Target.SIZE_ORIGINAL)
