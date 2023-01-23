6afb6ba5b8742a248a9d11699bba8045b8219c5a
Kamlesh Yadav <kamlesh.y@multitvsolution.com>
2023-01-23 18:25:43 +0530

    pause and resume downloads

--- app/src/main/java/com/multitv/ott/multitvvideoplayer/ExoVideoDownloadHelper.java
+++ app/src/main/java/com/multitv/ott/multitvvideoplayer/ExoVideoDownloadHelper.java
@@ -96,7 +96,7 @@ public class ExoVideoDownloadHelper implements DownloadTracker.Listener, SdkPopC
 
 
     public boolean isVideoStateResumeAndPause(String videoUrl, String videoTitle) {
-        return DownloadUtil.INSTANCE.getDownloadTracker(context).isVideoDownloadingResume(getMediaItem(videoUrl, videoTitle));
+        return DownloadUtil.INSTANCE.getDownloadTracker(context).isVideoDownloadingPauseAndResume(getMediaItem(videoUrl, videoTitle));
     }
 
 
