5691c451f787a87ccef25ccf05c14c2a565e98cb
vikramsharma306 <vikram.sharma@multitvsolution.com>
2023-03-10 21:38:28 +0530

    v

--- app/src/main/java/com/multitv/ott/multitvvideoplayer/BalajiVideoPlayer.kt
+++ app/src/main/java/com/multitv/ott/multitvvideoplayer/BalajiVideoPlayer.kt
@@ -1986,7 +1986,7 @@ class BalajiVideoPlayer(
 
 
         if (!TextUtils.isEmpty(parentalAge)) {
-            contentRatedTv.setText("Rated U/A " + parentalAge + "+")
+            contentRatedTv.setText("Rated U/A " + parentalAge)
             contentRatedTv.visibility = View.VISIBLE
         } else {
             contentRatedTv.visibility = View.GONE
